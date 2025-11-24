294bbf06aa71426ff7a0de17744fef0c3e07f9c2
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-11-24 17:03:28 -0300

    feat: add validation in username, email and password

--- src/main/java/br/com/patickcuppi/job_platform/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/patickcuppi/job_platform/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.patickcuppi.job_platform.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,14 @@ public class CandidateEntity {
 
   private UUID id;
   private String name;
+
+  @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can only contain letters, numbers, and underscores")
   private String username;
+
+  @Email(message = "Invalid email format")
   private String email;
+
+  @Length(min = 8, max = 100, message = "Password must be at least 8 characters long")
   private String password;
   private String description;
   private String curriculum;
