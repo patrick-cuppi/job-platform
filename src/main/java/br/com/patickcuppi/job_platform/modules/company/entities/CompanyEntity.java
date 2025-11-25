9affa9dad2b4a9a6bec0ea4d01b2876679a45249
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-11-25 20:49:04 -0300

    feat: create company entity

---
+++ src/main/java/br/com/patickcuppi/job_platform/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,44 @@
+package br.com.patickcuppi.job_platform.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private String name;
+
+  @NotBlank(message = "Username cannot be blank")
+  @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can only contain letters, numbers, and underscores")
+  private String username;
+
+  @Email(message = "Invalid email format")
+  private String email;
+
+  @Length(min = 8, max = 100, message = "Password must be at least 8 characters long")
+  private String password;
+
+  private String website;
+
+  private String description;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
