9cc246e78c38632a8700e77c61f5f12221c8ec39
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-11-25 17:06:08 -0300

    feat: create table candidates

--- src/main/java/br/com/patickcuppi/job_platform/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/patickcuppi/job_platform/modules/candidate/CandidateEntity.java
@@ -1,18 +1,27 @@
 package br.com.patickcuppi.job_platform.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   private String name;
 
   @NotBlank(message = "Username cannot be blank")
@@ -24,7 +33,11 @@ public class CandidateEntity {
 
   @Length(min = 8, max = 100, message = "Password must be at least 8 characters long")
   private String password;
+
   private String description;
+
   private String curriculum;
+
+  private LocalDateTime createdAt;
 
 }
