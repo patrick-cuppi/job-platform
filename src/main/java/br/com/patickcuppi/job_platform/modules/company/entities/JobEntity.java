9ada3ef1b674d530b799a7d0aaea4dfc85f659fa
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-11-26 18:23:36 -0300

    refactor: table job -> duplicated in mapping (company_id)

--- src/main/java/br/com/patickcuppi/job_platform/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/patickcuppi/job_platform/modules/company/entities/JobEntity.java
@@ -29,7 +29,7 @@ public class JobEntity {
   private String level;
 
   @ManyToOne()
-  @JoinColumn(name = "company_id")
+  @JoinColumn(name = "company_id", insertable = false, updatable = false)
   private CompanyEntity company;
 
   @Column(name = "company_id")
