0062be92ca4438a802daee35c7aa6b899e9ef44c
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-11-26 20:12:08 -0300

    feat: create job repository, use case and controller

---
+++ src/main/java/br/com/patickcuppi/job_platform/modules/company/useCases/CreateJobUseCase.java
@@ -0,0 +1,18 @@
+package br.com.patickcuppi.job_platform.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.patickcuppi.job_platform.modules.company.entities.JobEntity;
+import br.com.patickcuppi.job_platform.modules.company.repositories.JobRepository;
+
+@Service
+public class CreateJobUseCase {
+
+  @Autowired
+  private JobRepository jobRepository;
+
+  public JobEntity execute(JobEntity jobEntity) {
+    return this.jobRepository.save(jobEntity);
+  }
+}
