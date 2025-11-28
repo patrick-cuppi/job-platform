1775071925f6f9bba5a41feea5e4b1ffaffdccc0
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-11-28 18:43:23 -0300

    feat: hash password and create token-jwt for companies

--- src/main/java/br/com/patickcuppi/job_platform/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/patickcuppi/job_platform/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,6 +1,7 @@
 package br.com.patickcuppi.job_platform.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.patickcuppi.job_platform.exceptions.CompanyFoundException;
@@ -13,12 +14,18 @@ public class CreateCompanyUseCase {
   @Autowired
   private CompanyRepository companyRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public CompanyEntity execute(CompanyEntity companyEntity) {
     this.companyRepository
         .findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
         .ifPresent((company) -> {
           throw new CompanyFoundException();
         });
+
+    var password = passwordEncoder.encode(companyEntity.getPassword());
+    companyEntity.setPassword(password);
 
     return this.companyRepository.save(companyEntity);
   }
--- src/main/java/br/com/patickcuppi/job_platform/security/SecurityConfig.java
+++ src/main/java/br/com/patickcuppi/job_platform/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package br.com.patickcuppi.job_platform.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,11 +16,18 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> {
           auth
               .requestMatchers("/candidate/").permitAll()
-              .requestMatchers("/company/").permitAll();
+              .requestMatchers("/company/").permitAll()
+              .requestMatchers("/auth/company").permitAll()
+              .requestMatchers("/auth/candidate").permitAll();
 
           auth.anyRequest().authenticated();
         });
 
     return http.build();
+  };
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
