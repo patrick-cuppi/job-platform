4dfe5dd3e5717368760f886d7f2b09588c27bae1
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-12-04 18:07:30 -0300

    feat(swagger): create swagger config

---
+++ src/main/java/br/com/patickcuppi/job_platform/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package br.com.patickcuppi.job_platform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+
+    return new OpenAPI()
+        .info(new Info()
+            .title("Job Platform API")
+            .description("API for Job Platform Application")
+            .version("1.0.0"))
+        .schemaRequirement("jwt_auth", createSecurityScheme());
+  }
+
+  private SecurityScheme createSecurityScheme() {
+
+    return new SecurityScheme()
+        .name("jwt_auth")
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("bearer")
+        .bearerFormat("JWT");
+  }
+}
